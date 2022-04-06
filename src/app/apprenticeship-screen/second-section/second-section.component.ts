3668312773871792d2632b2eae88edb7d18f91b9
Supatsara <supatsara.rodratsa@gmail.com>
2022-04-06 18:28:21 +0700

    create card to show section 2 details

--- src/app/apprenticeship-screen/second-section/second-section.component.ts
+++ src/app/apprenticeship-screen/second-section/second-section.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SubCardModel } from 'src/app/shared/model/model';
+import { cardMockData } from './mock.constant';
 
 @Component({
   selector: 'app-second-section',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondSectionComponent implements OnInit {
 
+  public mockData: SubCardModel[] = [];  
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.mockData = cardMockData;
   }
 
 }
