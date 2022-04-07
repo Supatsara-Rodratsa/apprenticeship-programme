accf7eb104480a57a57794e7f8e48bcb11a0a28b
Supatsara <supatsara.rodratsa@gmail.com>
2022-04-08 04:01:55 +0700

    create bottom sticky bar

---
+++ src/app/shared/components/bottom-sticky-bar/bottom-sticky-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-sticky-bar',
+  templateUrl: './bottom-sticky-bar.component.html',
+  styleUrls: ['./bottom-sticky-bar.component.css']
+})
+export class BottomStickyBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
